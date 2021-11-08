c6d877e077352facfd789c25bdaa121ab26f7f7e
mhwahdab <moustafawahdan55@gmail.com>
2021-11-08 18:30:48 +0200

    removed unnecessary import

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
+
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         PlayFair cipherer = new PlayFair("monarchy");
         for (int i = 0; i < 5; i++) {
             for (int j =0; j < 5; j++)
